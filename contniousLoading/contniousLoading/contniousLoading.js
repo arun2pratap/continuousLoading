7daadfeb5f7603a2b0a3fbd5b0879b6908bb6d04
Neo <arun2pratap@gmail.com>
2015-08-17 11:52:27 +0530

    working example pushed.

--- contniousLoading/contniousLoading/contniousLoading.js
+++ contniousLoading/contniousLoading/contniousLoading.js
@@ -8,92 +8,47 @@
             isHTMLTable: false,
 			showEmptyCell: true
 	};
-    $.fn.expandCollapse = function( options ) { 
-        settings = $.extend({}, defaults, options );
-        var tempColTitle = '';
-        var colTitle = '';
-        var uniqueColTitle = [];
-        var elementExpCollap ;
-    	var evenRow = true;
-    	var rowIdentifier = settings.isjqGrid == true ? "tr.jqgrow" : "tr" ;
-        $(this).find(rowIdentifier).each(function(index,element){
-        	element = $(element);
-        	if(!settings.isjqGrid){
-        		element.find('td:eq('+(settings.colNumber-1)+')').attr("title",
-        				element.find('td:eq('+(settings.colNumber-1)+')').html());
-        		element.attr("id","ecr-"+index);
-        	}
-        	colTitle = element.find('td:eq('+(settings.colNumber-1)+')').attr("title");
-        	if(index == 0 || (tempColTitle != colTitle)){
-	        	elementExpCollap = element;
-	        	elementExpCollap.find('td:eq(0)').on('click', click);
-	        	elementExpCollap.attr('expand-collapse-ids','');
-	        	applyExpandCollapseIcon(elementExpCollap);
-	        	if(evenRow){
-	        		element.addClass(settings.oddRowClass);
-	        		evenRow = false;
-	        	}else{
-	        		element.addClass(settings.evenRowClass);
-	        		evenRow = true;
-	        	}	        	
-        	}else{
-        		var elementId = "#"+element.attr("id");
-        		var expandCollapseIds = elementExpCollap.attr('expand-collapse-ids');
-        		elementExpCollap.attr('expand-collapse-ids', elementId + "," + expandCollapseIds);
-        		if(settings.showEmptyCell){
-        			element.find('td:lt('+ settings.colNumber +')').html('');
-        		}
-        		element.css("display","none");
-        		if(evenRow){
-	        		element.addClass(settings.evenRowClass);
-	        	}else{
-	        		element.addClass(settings.oddRowClass);
-	        	}
-        	}
-        	tempColTitle = colTitle ;
-        });
-    };
-    
-    $.fn.expandAll = function(){
-    	expandCollapseAll(this,true);
-    }
-    
-    $.fn.collapseAll = function(){
-    	expandCollapseAll(this,false);
-    }
-    
-    var expandCollapseAll = function(element,isExpand){
-    	$(element).find('[expand-collapse-ids]').each(function(index,element){
-    		element = $(element);
-    		$.each(element.attr("expand-collapse-ids").split(','),function(index,value){
-    			value = $(value);
-    			if(isExpand){
-    				value.css('display','table-row');
-    			}else{value.css('display','none');}
-    		});
-    		element.find('td:eq(0) span:eq(0)').removeClass('expandIcon collapseIcon');
-    		if(isExpand){
-    			element.find('td:eq(0) span:eq(0)').addClass('expandIcon');
-    		}else{element.find('td:eq(0) span:eq(0)').addClass('collapseIcon');}
-    	});
-    }
-    
-	var click = function(ev){
-		var expandCollapseIds = $(this).parent().attr("expand-collapse-ids");
-		$.each(expandCollapseIds.split(","),function(index,value){
-			value = $(value);
-			if(value.css('display') == 'none'){
-				value.css('display','table-row');
-			}else{
-				value.css('display','none');
-			}
-		});
-		$(this).find('span:eq(0)').toggleClass('expandIcon collapseIcon');
-	}
 	
-	var applyExpandCollapseIcon = function(elementExpCollap){
-		var tdElement = elementExpCollap.find('td:eq(0)');
-		tdElement.prepend('<span style="display:inline-block" class="expandCollapseSprites collapseIcon"></span>');
-	}
+    $.fn.continousLoading = function( options ) {
+    	console.log((this));
+    	var $gridTableObj = (this);
+    		if(options.continousLoading.timeoutVar){clearTimeout(options.continousLoading.timeoutVar);}
+    		var JSONData = options.data ;
+    		var slicedArray = [];
+    		var dataCount = JSONData.length;
+    		var startIndex = 0;
+    		var split = options.continousLoading.split;
+    		var endIndex = options.continousLoading.firstPageSize; 
+    		slicedArray = JSONData.slice(startIndex,endIndex);
+    		startIndex = endIndex ;
+    		endIndex = endIndex + split ;
+    		options.data = slicedArray;
+    		var loadCompleteFunc = options.loadComplete;
+    		options.loadComplete = function(data){
+    			loadCompleteFunc(data);
+				setTimeout(function() {
+					clientSideContiniousLoading(JSONData, dataCount, startIndex, endIndex, split);
+				},options.continousLoading.timeOut);
+    		}    		
+    		$gridTableObj.jqGrid(options);
+    	
+    	function clientSideContiniousLoading(JSONData, dataCount, startIndex, endIndex, split){
+    			if(dataCount > startIndex){
+    					options.continousLoading.timeoutVar = setInterval(function() {
+    						var slicedArray;
+    						slicedArray = JSONData.slice(startIndex,endIndex);
+    						if(slicedArray.length == 0){
+    							clearTimeout(options.continousLoading.timeoutVar);
+    							loadCompleteFunc();
+    						}
+    						console.log('endIndex',endIndex);
+    						$gridTableObj.addRowData(undefined, slicedArray);
+    						startIndex = endIndex ;
+    						endIndex = endIndex + split ;
+    					}, options.continousLoading.timeOut);
+    			}
+    					
+    	};
+    };
  
 }( jQuery ));
